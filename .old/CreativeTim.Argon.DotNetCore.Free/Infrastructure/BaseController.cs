4ea08544c467e45ac9a6403c2b30915f9558e1c8
Thomas-Blakeney <blaumc@icloud.com>
2021-03-06 17:28:30 +0100

    Deleted old Files

--- .old/CreativeTim.Argon.DotNetCore.Free/Infrastructure/BaseController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace CreativeTim.Argon.DotNetCore.Free.Infrastructure
-{
-    [Route("[controller]/[action]", Name = "[controller]_[action]")]
-    public abstract class BaseController : Controller
-    {
-    }
-}
--- .old/CreativeTim.Argon.DotNetCore.Free/Infrastructure/ErrorHandling/ModelStateTransfer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
-
-namespace CreativeTim.Argon.DotNetCore.Free.Infrastructure.ErrorHandling
-{
-    public abstract class ModelStateTransfer : ActionFilterAttribute
-    {
-        protected const string Key = nameof(ModelStateTransfer);
-    }
-}

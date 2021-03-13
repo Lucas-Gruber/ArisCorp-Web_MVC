fa60476d794931496347802ba3c5ce9df7e4483c
Thomas-Blakeney <blaumc@icloud.com>
2021-03-13 01:20:35 +0100

    Update bac 3

--- ArisCorpWeb/Controllers/VerseExkursController.cs
+++ ArisCorpWeb/Controllers/VerseExkursController.cs
@@ -19,11 +19,45 @@ namespace ArisCorpWeb.Controllers
         {
             return View();
         }
+        public ActionResult Starmap()
+        {
+            return View();
+        }
 
-        [Route ("VerseExkurs/Systeme/{System}")]
+        [Route ("VerseExkurs/Starmap/{System}")]
         public IActionResult System(string System)
         {
             return View(System);
+        }
+        public ActionResult Technologien()
+        {
+            return View();
+        }
+
+        [Route("VerseExkurs/Technologien/{Technologie}")]
+        public IActionResult Technologien(string Technologie)
+        {
+            return View(Technologie);
+        }
+        public ActionResult Firmen()
+        {
+            return View();
+        }
+
+        [Route("VerseExkurs/Firmen/{Firma}")]
+        public IActionResult Firmen(string Firma)
+        {
+            return View(Firma);
+        }
+        public ActionResult Alienrassen()
+        {
+            return View();
+        }
+
+        [Route("VerseExkurs/Alienrassen/{Alienrasse}")]
+        public IActionResult Alienrassen(string Alienrasse)
+        {
+            return View(Alienrasse);
         }
     }
 }

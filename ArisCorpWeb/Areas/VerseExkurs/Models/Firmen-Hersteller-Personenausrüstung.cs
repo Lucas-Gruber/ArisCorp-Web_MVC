9b59c140fbd5ca69bafaa8e5bac8510cd2ae7e3d
GruberL <admin@firecave.de>
2022-09-15 21:16:26 +0200

    added dockerfile

---
+++ ArisCorpWeb/Areas/VerseExkurs/Models/Firmen-Hersteller-Personenausrüstung.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArisCorpWeb.Models
+{
+    public class Firmen_Hersteller_Personenausrüstung
+    {
+        [DataType(DataType.Html)]
+        public string Id { get; set; }
+        public string Firma { get; set; }
+
+        [DataType(DataType.Html)]
+        public string PictureEinleitung { get; set; }
+        [DataType(DataType.Html)]
+        public string Einleitung { get; set; }
+
+        
+        [DataType(DataType.Html)]
+        public string PictureContent { get; set; }
+        [DataType(DataType.Html)]
+        public string Content { get; set; }
+    }
+}
